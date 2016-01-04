2d107023316fa39c9b8cdc7f2f0bd8877694bd65
yue-b <yue@byuedeMacBook-Pro.local>
2016-01-04 16:33:36 +0800

    注册基本完成

--- com/org/Register.java
+++ com/org/Register.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,12 +17,6 @@ import com.mysql.jdbc.ResultSet;
 import com.mysql.jdbc.Statement;
 
 public class Register extends HttpServlet {
-//	protected void doGet(HttpServletRequest request,
-//			HttpServletResponse response) throws ServletException, IOException {
-//		response.setContentType("text/json;charset=UTF-8");
-//        PrintWriter out = response.getWriter();
-//        out.write("hello");
-//	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
@@ -31,34 +26,43 @@ public class Register extends HttpServlet {
 		String phone = request.getParameter("phone");
 		String email = request.getParameter("email");
 		String password0 = request.getParameter("password0");
-		String portail = "/Users/yue/Documents/workspace/java/BookStore/WebContent/images/close_1.png";
+		String portail = "/Users/yue/Documents/workspace/java/BookStore/WebContent/portait/default_portait.jpg";
+		String uid = UUID.randomUUID().toString().replaceAll("-", "");;
+		System.out.println(uid);
 		
-		System.out.println("insert into use_info (uname,phone,email,password,portail) values (\""
-				+userName+"\",\""+phone+"\",\""+email+"\",\""+password0+"\",\""+portail+"\");");
-		String sql = "insert into use_info (uname,phone,email,password,portail) values (\""
-				+userName+"\",\""+phone+"\",\""+email+"\",\""+password0+"\",\""+portail+"\");";
+		String sql = "insert into use_info (uid,uname,phone,email,password,portail) values (\""
+				+uid+"\",\""+userName+"\",\""+phone+"\",\""+email+"\",\""+password0+"\",\""+portail+"\");";
 		ConnectDatabase con_data = new ConnectDatabase();
 		con_data.connect();
+		
 		try {
-			con_data.pst = con_data.connection.prepareStatement(sql);
-			out.print("");
-			System.out.println("成功");
+			con_data.pst = con_data.connection.prepareStatement("select * from use_info where phone=\""+phone+"\";");//准备执行SQL语句
+			con_data.ret = (ResultSet) con_data.pst.executeQuery();
+			if(!con_data.ret.next())
+			{
+				con_data.ret.close();
+				try {
+					con_data.pst = con_data.connection.prepareStatement(sql);
+					con_data.pst.executeUpdate();
+					con_data.pst.close();
+					out.print("{\"msg\":\"注册成功！\"}");
+					System.out.println("数据库插入成功！");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					out.print("{\"msg\":\"注册失败！\"}");
+					System.out.println("数据库插入失败！");
+				}
+			}
+			else {
+				out.print("{\"msg\":\"手机号已注册！\"}");
+				System.out.println("手机号已注册!");
+			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("插入失败");
+			System.out.println("数据库查询失败！");
 		}
-		try {
-			con_data.pst.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		try {
-			con_data.pst.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		
 	}
 	
